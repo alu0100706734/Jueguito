9aa659796b566c7477f1b8e073e65b033fa7af3f
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2015-12-08 17:46:58 +0000

    añadida velocidad de balas, jugador, enemigo y key input

--- js/canvas.js
+++ js/canvas.js
@@ -57,6 +57,12 @@ var terrainPattern;
 var score = 0;
 var scoreEl = document.getElementById('score');
 
+
+var playerSpeed = 200;
+var bulletSpeed = 500;
+var enemySpeed = 100;
+
+
 function update(dt) {
     gameTime += dt;
 
@@ -76,4 +82,43 @@ function update(dt) {
 
     scoreEl.innerHTML = score;
 };
+
+
+function handleInput(dt) {
+    if(input.isDown('DOWN') || input.isDown('s')) {
+        player.pos[1] += playerSpeed * dt;
+    }
+
+    if(input.isDown('UP') || input.isDown('w')) {
+        player.pos[1] -= playerSpeed * dt;
+    }
+
+    if(input.isDown('LEFT') || input.isDown('a')) {
+        player.pos[0] -= playerSpeed * dt;
+    }
+
+    if(input.isDown('RIGHT') || input.isDown('d')) {
+        player.pos[0] += playerSpeed * dt;
+    }
+
+    if(input.isDown('SPACE') &&
+       !isGameOver &&
+       Date.now() - lastFire > 100) {
+        var x = player.pos[0] + player.sprite.size[0] / 2;
+        var y = player.pos[1] + player.sprite.size[1] / 2;
+
+        bullets.push({ pos: [x, y],
+                       dir: 'forward',
+                       sprite: new Sprite('img/sprites.png', [0, 39], [18, 8]) });
+        bullets.push({ pos: [x, y],
+                       dir: 'up',
+                       sprite: new Sprite('img/sprites.png', [0, 50], [9, 5]) });
+        bullets.push({ pos: [x, y],
+                       dir: 'down',
+                       sprite: new Sprite('img/sprites.png', [0, 60], [9, 5]) });
+
+
+        lastFire = Date.now();
+    }
+}
 
