6c6b6679ac5352d4aef09c3b820faeda39c56944
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2015-12-08 17:14:57 +0000

    creando jugador, juego y puntuacion

--- js/canvas.js
+++ js/canvas.js
@@ -17,3 +17,43 @@ function main() {
     lastTime = now;
     requestAnimFrame(main);
 };
+
+
+function init() {
+    terrainPattern = ctx.createPattern(resources.get('img/terreno.png'), 'repeat');
+
+    document.getElementById('replay').addEventListener('click', function() {
+        reset();
+    });
+
+    reset();
+    lastTime = Date.now();
+    main();
+}
+
+
+resources.load([
+    'img/sprites.png',
+    'img/terrain.png'
+]);
+resources.onReady(init);
+
+
+var player = {
+    pos: [0, 0],
+    sprite: new Sprite('img/sprites.png', [0, 0], [39, 39], 16, [0, 1])
+};
+
+var bullets = [];
+var enemies = [];
+var explosions = [];
+
+var lastFire = Date.now();
+var gameTime = 0;
+var isGameOver;
+var terrainPattern;
+
+
+var score = 0;
+var scoreEl = document.getElementById('score');
+
