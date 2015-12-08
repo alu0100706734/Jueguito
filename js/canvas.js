95386ba1a085a4802fdfc73a2f73ad0068a1f8ab
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2015-12-08 16:59:49 +0000

    creando lienzo y ciclo continuo

---
+++ js/canvas.js
@@ -0,0 +1,19 @@
+
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main);
+};
