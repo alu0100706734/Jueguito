0816a72c27056368f7afe86429a4ef80d767c738
Juan Nicolas <juannicolasvargasmoreno@hotmail.com>
2015-12-08 17:32:42 +0000

    metodo render objeto sprite

--- js/sprite.js
+++ js/sprite.js
@@ -9,3 +9,33 @@ function Sprite(url, pos, size, speed, frames, dir, once) {
     this.once = once;
 };
 
+
+	Sprite.prototype = {
+	   update: function(dt){
+		this._index += this.speed*dt;
+	   }
+	   
+	   render: function(ctx){
+	   	var frame;
+	   	if (this.speed >0) {
+	   	   var max = this.frames.lengt;
+	   	   var idx = Math.floor(this._index);
+	   	   frame = this.frames[idx % max];
+	   	
+	   	   if (this.once && idx>=max){
+	   		this.done = true;
+	   		return;
+	  	   }
+                }
+		else {
+		    frame = 0;
+		}
+		
+		ctx.drawImage(resources.get(this.url),
+                  x, y,
+                  this.size[0], this.size[1],
+                  0, 0,
+                  this.size[0], this.size[1]);
+	  }
+	};
+
